f10bb0f44cb1c5c099cab7d2e86b125c2c80b695
Anton <astakheyko@bk.ru>
2026-09-09 20:48:36 +0300

    Init

---
+++ Server/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+
+
+app.Run();
+
